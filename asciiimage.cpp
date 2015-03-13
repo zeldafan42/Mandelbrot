dd5883f22c565feb3c591130cb16278dc2520bfc
Zombieranke <abcdefghijklmnop@qrs>
2015-03-13 12:48:27 +0100

    AsciiImage class prototype created

--- asciiimage.cpp
+++ asciiimage.cpp
@@ -1,11 +1,50 @@
-/*
- * asciiimage.cpp
- *
- *  Created on: 13.03.2015
- *      Author: Christoph Majcen
- *      		Arthur Bouz
- */
+#include <iostream>
+
+class AsciiImage
+{
+	private:
+		int length;
+		int width;
+		char* image;
+
+	public:
+		AsciiImage(int length,int width)
+		{
+			this->length = length;
+			this->width = width;
+			char* image = new char[length*width];
+		};
 
 
+		void setPix(int x, int y, int color)
+		{
+			if(color>70)
+			{
+				color = 70;
+			}
+			if(color<0)
+			{
+				color = 0;
+			}
+			char a = seventyShadesOfGrey[color];
 
+			image[(length-1)*(y-1)+x] = a;
+		};
+
+		void show()
+		{
+			int i = 0;
+			int j = 0;
+			for(i=0;i<width;i++)
+			{
+				for(j=0;j<length;j++)
+				{
+					std::cout << image[(i*(length-1)+j)];
+				}
+				std::cout  << std::endl;
+			}
+		};
+
+
+};
 
